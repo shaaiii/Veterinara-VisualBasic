2ad369efc63e1a80c1b2b1c35b5e69f34ca35fc6
shaaiii <sdelossantos@loopstud.io>
2020-07-15 12:10:28 -0300

    persistencia

--- Veterinaria/Persistencia/PConexion.vb
+++ Veterinaria/Persistencia/PConexion.vb
@@ -1,3 +1,17 @@
 ﻿Public Class PConexion
+    Public Function OpenConexion() As Npgsql.NpgsqlConnection
+        Dim conexion As Npgsql.NpgsqlConnection
+        Try
+            conexion = New Npgsql.NpgsqlConnection()
+            Dim cadenaDeConexion As String
+            cadenaDeConexion = "Server=127.0.0.0;Port=5432;User Id='postgres';Password='root';Database=ejemplo"
+            conexion.ConnectionString = cadenaDeConexion
+            conexion.Open()
+        Catch ex As Exception
+            conexion.Close()
+            Throw ex
+        End Try
+        Return conexion
+    End Function
 
 End Class
--- Veterinaria/Persistencia/PPersonas.vb
+++ Veterinaria/Persistencia/PPersonas.vb
@@ -1,3 +1,27 @@
 ﻿Public Class PPersonas
+    Dim conexion As New Npgsql.NpgsqlConnection()
+    Public Sub altaPersona(personita As Personas)
+        Try
+            Dim clasecnn = New PConexion
+            conexion = clasecnn.OpenConexion()
 
+            Dim cmd = New Npgsql.NpgsqlCommand()
+            cmd.Connection = conexion
+
+            Dim cadenaComando = "INSERT INTO persona (ci, nombre, direccion) VALUES(@ci, @nombre, @direccion);"
+            cmd.CommandText = cadenaComando
+
+            cmd.Parameters.Add("@ci", NpgsqlTypes.NpgsqlDbType.Integer).Value = personita.Ci
+            cmd.Parameters.Add("@nombre", NpgsqlTypes.NpgsqlDbType.Varchar, 50).Value = personita.Nombre
+            cmd.Parameters.Add("@direccion", NpgsqlTypes.NpgsqlDbType.Varchar, 50).Value = personita.Direccion
+            Dim resultado As Integer
+
+            resultado = cmd.ExecuteNonQuery()
+
+        Catch ex As Exception
+            Throw ex
+        Finally
+            conexion.Close()
+        End Try
+    End Sub
 End Class
